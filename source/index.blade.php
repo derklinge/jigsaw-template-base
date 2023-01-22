2f0b4009d76b7bb9e6989f67e9455bf1950b1a52
Sebastian Klinge <github@derklinge.de>
2023-01-22 17:25:08 +0100

    Add a self-hosted custom web font example
    Signed-off-by: Sebastian Klinge <github@derklinge.de>

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('body')
 <div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+    <h1 class="text-3xl font-sans">Hello font-sans!</h1>
+    <h2 class="text-3xl font-custom-barlow">Hello font-custom-barlow!</h1>
 </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     '!source/**/_tmp/*' // exclude temporary files
   ],{ dot: true }),
   theme: {
-    extend: {},
+    extend: {
+      // Extend the default theme with custom fonts here.
+      //
+      // CSS class names are prefixed with `font-` automatically.
+      // Also see 'source/_assets/css/main.css' for font definition processing.
+      //
+      fontFamily: {
+        'custom-barlow': ['Barlow', ...defaults.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
